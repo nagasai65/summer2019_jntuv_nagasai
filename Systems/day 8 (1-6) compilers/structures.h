63779696508f9c292a739c823d1cc3f8c8cba9f5
Nagasai Kollipara <32226487+nagasai65@users.noreply.github.com>
2019-08-26 15:09:23 +0530

    unwanted deleted

---
+++ Systems/day 8 (1-6) compilers/structures.h
@@ -0,0 +1,41 @@
+#define NAMELEN 20
+struct SymbolTable
+{
+	char name[NAMELEN];
+	int address;
+	int size;
+	struct SymbolTable *next;
+};
+struct LabelTable
+{
+	char blockname[NAMELEN];
+	int address;
+	struct LabelTable *next;
+};
+struct Stack
+{
+	int instruction_num;
+	struct Stack *next;
+};
+struct ConstValues
+{
+	int address;
+	int value;
+	struct ConstValues *next;
+};
+struct Collection
+{
+	struct SymbolTable *symboltable;
+	struct ConstValues *const_values;
+	int noofconstantvalues;
+	int noofinstructions;
+	int **instrtab;
+	int current_memory_address;
+	struct LabelTable *labeltable;
+	struct Stack *stack;
+};
+struct PreserveOnFile
+{
+	int noof_instructions;
+	int noof_constant_values;
+};
